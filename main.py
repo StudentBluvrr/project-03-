ef6182e764af1fdc4288856b2959c9624a2f60fd
BluvrrStudent <98335700+StudentBluvrr@users.noreply.github.com>
2022-01-27 13:33:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def check(password):
+  has_number = False
+  for i in password:
+    if i.isdigit():
+      has_number = True
+  return has_number
+
+
+password = input("password: ")
+has_number = check(password)
+print(has_number)
